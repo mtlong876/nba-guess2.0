4317db89814c26ea33dd8a569d32611e78d0a353
mtlong876 <104344592+mtlong876@users.noreply.github.com>
2025-07-08 01:07:14 +1000

    eslint :D

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ const eslintConfig = [
     // Note: you must disable the base rule as it can report incorrect errors
     rules: {
       "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "error"
+      "@typescript-eslint/no-unused-vars": "off"
     }
   }
 ];
