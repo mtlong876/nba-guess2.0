ab7f635852887ae31634e8c93ac0de1e4ecbf09a
mtlong876 <104344592+mtlong876@users.noreply.github.com>
2025-07-08 00:56:13 +1000

    eslint test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.rules({
+    "no-unused-vars": ["warn", // Warn about unused variables
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: true,
+      },
+    ],
+  }),  
 ];
 
 export default eslintConfig;
